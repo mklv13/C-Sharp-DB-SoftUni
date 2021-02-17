9a2df0648a779afa92375ee037ba7c4b5885947d
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-02-17 22:11:50 +0200

    Add files via upload

---
+++ MSSQL/04. Built In Functions/User Email Providers.sql
@@ -0,0 +1,9 @@
+USE Diablo
+
+SELECT Username
+       SUBSTRING(Email,
+       CHARINDEX('@', Email) + 1,
+       LEN(Email) - CHARINDEX('@', Email) + 1)
+       AS [Email Provider]
+	FROM Users
+	ORDER BY [Email Provider], Username
