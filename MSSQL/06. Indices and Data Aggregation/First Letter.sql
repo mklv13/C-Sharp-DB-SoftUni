4542feb43f4a837f2cbb2701bb007927e80fd7e3
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-02-24 22:20:47 +0200

    Add files via upload

---
+++ MSSQL/06. Indices and Data Aggregation/First Letter.sql
@@ -0,0 +1,9 @@
+USE Gringotts
+
+SELECT FirstLetter
+FROM (
+         SELECT SUBSTRING(FirstName, 1, 1) AS FirstLetter
+         FROM WizzardDeposits
+         WHERE DepositGroup LIKE 'Troll Chest') AS FirstCharQuery
+GROUP BY FirstLetter
+ORDER BY FirstLetter
